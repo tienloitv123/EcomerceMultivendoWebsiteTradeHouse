09fae359368ac9f4c8b20f48b2743e1e76d2e613
tienloitv123 <lamtienloitv@gmail.com>
2024-07-06 21:38:01 +0700

    Create model, guard and make migration

--- config/auth.php
+++ config/auth.php
@@ -17,6 +17,20 @@ return [
         'guard' => 'web',
         'passwords' => 'users',
     ],
+    'admin'=>[
+        'driver'=>'eloquent',
+        'model'=>App\Models\Admin::class,
+    ],
+
+    'client'=>[
+        'driver'=>'eloquent',
+        'model'=>App\Models\Client::class,
+    ],
+
+    'seller'=>[
+        'driver'=>'eloquent',
+        'model'=>App\Models\Seller::class,
+    ],
 
     /*
     |--------------------------------------------------------------------------
@@ -39,6 +53,20 @@ return [
         'web' => [
             'driver' => 'session',
             'provider' => 'users',
+        ],
+        'admin' => [
+            'driver' => 'session',
+            'provider' => 'admins',
+        ],
+
+        'client' => [
+            'driver' => 'session',
+            'provider' => 'clients',
+        ],
+
+        'seller' => [
+            'driver' => 'session',
+            'provider' => 'sellers',
         ],
     ],
 
@@ -65,6 +93,20 @@ return [
             'model' => App\Models\User::class,
         ],
 
+        'admins' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Admin::class,
+        ],
+
+        'clients' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Client::class,
+        ],
+
+        'sellers' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Seller::class,
+        ],
         // 'users' => [
         //     'driver' => 'database',
         //     'table' => 'users',
@@ -96,6 +138,18 @@ return [
             'table' => 'password_reset_tokens',
             'expire' => 60,
             'throttle' => 60,
+        ],
+        'admins'=>[
+            'driver'=>'eloquent',
+            'model'=>App\Models\Admin::class,
+        ],
+        'clients'=>[
+            'driver'=>'eloquent',
+            'model'=>App\Models\Client::class,
+        ],
+        'sellers'=>[
+            'driver'=>'eloquent',
+            'model'=>App\Models\Seller::class,
         ],
     ],
 
--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
+            $table->string('guard')->nullable();
             $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
+            $table->timestamp('created_at')->nullable();});
     }
 
     /**
